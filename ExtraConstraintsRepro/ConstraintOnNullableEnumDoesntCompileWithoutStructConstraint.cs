c71e9185d8c586618eb66f99e74c88fd7f6eeecc
bessgeor <georgy.b@webkontrol.ru>
2017-09-11 18:23:42 +0300

    Added repro 2 (breaks build)

---
+++ ExtraConstraintsRepro/ConstraintOnNullableEnumDoesntCompileWithoutStructConstraint.cs
@@ -0,0 +1,22 @@
+﻿using ExtraConstraints;
+
+namespace ExtraConstraintsRepro
+{
+    internal class ConstraintOnNullableEnumDoesntCompileWithoutStructConstraint
+	{
+		public void DoRight<[EnumConstraint] TEnum>( TEnum param )
+		{
+
+		}
+
+		public void DoWrong1<[EnumConstraint] TEnum>( TEnum? param ) where TEnum : struct // [EnumConstraint] should be enough
+		{
+
+		}
+
+		public void DoWrong2<[EnumConstraint] TEnum>( TEnum? param )
+		{
+
+		}
+	}
+}
