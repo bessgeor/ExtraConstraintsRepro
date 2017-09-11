4d399440e64a160cb4e11ce7d71894b2d9bc2548
bessgeor <georgy.b@webkontrol.ru>
2017-09-11 18:24:11 +0300

    Added repro 1

---
+++ ExtraConstraintsRepro/ConstraintJustDoesntWork.cs
@@ -0,0 +1,24 @@
+﻿using ExtraConstraints;
+
+namespace ExtraConstraintsRepro
+{
+    internal class ConstraintJustDoesntWork
+    {
+	    public enum Version
+	    {
+		    V1,
+			V2
+		}
+
+	    public void DoRight( Version version )
+		    => DoInternal( version );
+
+		public void DoWrong( int number ) // builds succesfully while should not
+			=> DoInternal( number );
+
+		private void DoInternal<[EnumConstraint] TEnum>( TEnum thing )
+		{
+			
+		}
+	}
+}
